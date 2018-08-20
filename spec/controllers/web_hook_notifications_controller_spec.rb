43e5dbf0009e9594357e705c13531a247cfbc600
Alex Levine <a17levine@gmail.com>
2018-08-20 00:20:54 -0500

    Missed a few checks

--- spec/controllers/web_hook_notifications_controller_spec.rb
+++ spec/controllers/web_hook_notifications_controller_spec.rb
@@ -22,7 +22,19 @@ RSpec.describe WebHookNotificationsController, type: :controller do
 
     it "iterates the counters on the words hello and world" do
       word_table_ids = Word.where(text: ['hello', 'world']).pluck(:id)
-      expect(AccountWord.where(word_id: word_table_ids).pluck(:total_count)).to eq([1,1])
+      account = Account.find_by(name: 'alevee')
+      expect(AccountWord.where(word_id: word_table_ids, account_id: account.id).pluck(:total_count)).to eq([1,1])
+    end
+
+    it "saves the user" do
+      expect(Account.find_by(name: 'alevee')).to_not be_nil
+    end
+
+    it "saves the WebHookNotification" do
+      account = Account.find_by(name: 'alevee')
+      expect(WebHookNotification.where(account_id: account.id).last.message_text).to eq("Hello world!")
+      expect(WebHookNotification.where(account_id: account.id).last.message_occurred_at < 1.day.ago).to eq(true)
+      expect(WebHookNotification.where(account_id: account.id).last.raw_body_of_webhook_request).to_not be_nil
     end
   end
 
